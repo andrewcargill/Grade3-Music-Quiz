b594ba73ad7b09709b124294e3a14a44828e13d3
andrewcargill <andycargill01@aol.com>
2022-09-19 08:26:34 +0000

    Add state object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
+const state = {
+    questionNumber: 1, 
+    score: 0,
+    questions: [
+        {
+            title: 'A guitar has 5 strings!',
+            answer: false,
+            playerAnswer: null,
+        },
+        {
+            title: 'A banjo is a string instrument!',
+            answer: true,
+            playerAnswer: null,
+        },
+    ]
+}
+
 
