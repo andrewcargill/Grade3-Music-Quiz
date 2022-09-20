c23520e3578305af28ffa18b906b7a773f4d5da2
andrewcargill <andycargill01@aol.com>
2022-09-20 06:57:38 +0000

    Display question no. in ref to state object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,5 @@
 
 
-/*Question number HTML container*/
-let questionNumberHTML = document.getElementById("question-num");
-questionNumberHTML.innerHTML = "Question:";
-
-/*Display Question */
-let questionTextHTML = document.getElementById("text");
-questionTextHTML.innerHTML = "This is to test that JS is talking to the question container";
-
-//Button event listeners
-let buttonTrue = document.getElementById("true");
-buttonTrue.addEventListener("click", function() {
-});
-
-let buttonFalse = document.getElementById("false");
-buttonFalse.addEventListener("click", function() {
-});
-
 const state = {
     questionNumber: 1, 
     score: 0,
@@ -34,6 +17,26 @@ const state = {
     ]
 }
 
-console.log(state.questionNumber);
+
+/*Question number HTML container*/
+let questionNumberHTML = document.getElementById("question-num");
+questionNumberHTML.innerHTML = "Question: " + state.questionNumber;
+
+/*Display Question */
+let questionTextHTML = document.getElementById("text");
+questionTextHTML.innerHTML = "This is to test that JS is talking to the question container";
+
+//Button event listeners
+let buttonTrue = document.getElementById("true");
+buttonTrue.addEventListener("click", function() {
+});
+
+let buttonFalse = document.getElementById("false");
+buttonFalse.addEventListener("click", function() {
+});
+
+
+
+console.log(questionNumberHTML);
 
 
