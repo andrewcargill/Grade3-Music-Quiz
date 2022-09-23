ae439f4450d0cea0aece28eedbd57a02d575945a
andrewcargill <andycargill01@aol.com>
2022-09-23 20:57:29 +0000

    Adds username field and start button to landing page

---
+++ assets/js/landing.js
@@ -0,0 +1,6 @@
+let startGame = document.getElementById("start-game");
+startGame.addEventListener("click", function() {
+    window.location.href = "game.html";
+    console.log('------------hello');
+});
+
