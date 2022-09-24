da44c6433d19db8c7f5062cd4ff8b115ca51fe6d
andrewcargill <andycargill01@aol.com>
2022-09-24 06:06:21 +0000

    will use js to update landing screen text

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -2,11 +2,37 @@
 let username = document.getElementById("username");
 let players_username = username.value;
 
+
+/// start up
+
+document.addEventListener("DOMContentLoaded", function () {
+   welcomeHTML();
+   ///addListenersToButtons();
+});
+
+
+///Welcome text
+function welcomeHTML() {
+   let textContainer = document.getElementById("text");
+   textContainer.style.textAlign = "center";
+   textContainer.innerHTML =`
+   <p>
+   Hello and Welcome!
+</p>
+<p>This game will test your music knowledge.</p>
+<p>First off, enter a username! </p>
+   
+   `;
+};
+
+
+
+/// Listeners
 let startGame = document.getElementById("start-game");
 startGame.addEventListener("click", function() {
 
    window.localStorage.setItem("username", username.value);
-   window.location.href = "game.html";
+   ///window.location.href = "game.html";
     
 });
 
